983e5ab8303a3a4246aac4d32dced02f277870a7
ThinhDinhTran2001 <tincan@umich.edu>
2023-04-13 16:35:25 -0400

    updating positive and negative

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -11,38 +11,22 @@ import org.junit.Test;
 
 public class testSelectionSort {
 	@Test
-	public void test() {
-		testPositive();
-		testNegative();
-		testMixed();
-		testDuplicates();
+	public void testPositive() {
+	    int[] arr = {8, 9, 7, 10, 2};
+	    int[] expected = {2, 7, 8, 9, 10};
+	    SelectionSort positive = new SelectionSort();
+	    positive.basicSelectionSort(arr);
+	    assertArrayEquals(expected, arr);
 	}
 
-    public testSelectionSort() {
-    }
-    
-    public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
-        
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
-    }
-    
-    public void testNegative(){
-        
-        /** Test data contains negative values only **/
-    }
+	@Test
+	public void testNegative() {
+	    int[] arr = {-8, -9, -7, -10, -2};
+	    int[] expected = {-10, -9, -8, -7, -2};
+	    SelectionSort negative = new SelectionSort();
+	    negative.basicSelectionSort(arr);
+	    assertArrayEquals(expected, arr);
+	}
     
     public void testMixed(){
         
